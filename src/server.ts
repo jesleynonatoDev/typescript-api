d2acf6e91f0f86b6384ad90f69c2c8181fdda2bb
Jes Nonato <jesleynonato@hotmail.com>
2022-05-30 21:40:18 -0300

    setup server and create controller forecast

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
     constructor(private port = 3000) {
@@ -9,9 +10,15 @@ export class SetupServer extends Server {
 
     public init(): void {
         this.setupExpress();
+        this.setupController();
     }
 
     private setupExpress(): void {
         this.app.use(bodyParser.json());
+    }
+
+    private setupController(): void {
+        const forecastController = new ForecastController();
+        this.addControllers([forecastController]);
     }
 }
