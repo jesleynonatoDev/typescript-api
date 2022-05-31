8508ecc93a2537c8f726b36983e880e146d64b1d
Jes Nonato <jesleynonato@hotmail.com>
2022-05-31 20:30:56 -0300

    setup prettier

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,25 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
-    constructor(private port = 3000) {
-        super();
-    }
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupController();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupController();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupController(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
