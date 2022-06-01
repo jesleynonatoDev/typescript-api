0178e30272ba4066c5dcea6e33ae30aa7db462a9
Jes Nonato <jesleynonato@hotmail.com>
2022-06-01 20:52:19 -0300

    interfaces

--- src/clientes/stormGlass.ts
+++ src/clientes/stormGlass.ts
@@ -1,4 +1,21 @@
 import { AxiosStatic } from "axios";
+
+export interface StormGlassPointSource {
+    [key: string]: number
+}
+export interface StormGlassPoint {
+    readonly time: string;
+    readonly waveHeight: StormGlassPointSource;
+    readonly swellDirection: StormGlassPointSource;
+    readonly swellPeriod: StormGlassPointSource;
+    readonly swellHeight: StormGlassPointSource;
+    readonly waveDirection: StormGlassPointSource;
+    readonly windDirection: StormGlassPointSource;
+    readonly windSpeed: StormGlassPointSource;
+}
+export interface StormGlassForecastResponse {
+    hours: StormGlassPoint[];
+}
 export class StormGlass {
 
     readonly stormGlassAPIParams =
@@ -9,7 +26,7 @@ export class StormGlass {
 
     public async fetchPoints(ltd: number, lng: number): Promise<{}> {
         return this.request.get(
-            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+            `https://api.stormglass.io/v2/weather/point?lat=${ltd}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
         );
     }
 }
