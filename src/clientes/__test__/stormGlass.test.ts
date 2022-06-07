a1ffa3511dcd41a0bbf02b43d41fdaac1425c117
Jes Nonato <jesleynonato@hotmail.com>
2022-06-06 22:31:15 -0300

    add test case

--- src/clientes/__test__/stormGlass.test.ts
+++ src/clientes/__test__/stormGlass.test.ts
@@ -6,7 +6,9 @@ import axios from 'axios';
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+
   const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
@@ -16,5 +18,27 @@ describe('StormGlass client', () => {
     const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormglassNormalizedResponseFixture);
+  });
+
+  it('should exclude incomplete data points', async() => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    const incompleteResponse = {
+        hours: [
+            {
+                windDirection: {
+                    noaa: 300,
+                },
+                time: '2020-04-25T00:00:00+00:00'
+            },
+        ],
+    };
+    mockedAxios.get.mockResolvedValue({data: incompleteResponse});
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual({})
   });
 });
